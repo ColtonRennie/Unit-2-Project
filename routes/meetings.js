8d442ccbc20b72c727e64800f8cbf3ebbbda84fd
Colton Rennie <colton.rennie@hotmail.com>
2022-08-30 19:20:44 -0300

    updated html

--- routes/meetings.js
+++ routes/meetings.js
@@ -3,5 +3,11 @@ var router = express.Router()
 var meetingsCtrl = require('../controllers/meetings')
 
 router.get('/', meetingsCtrl.index)
+router.get('/new', meetingsCtrl.new)
+router.get('/:id', meetingsCtrl.show)
+router.post('/', meetingsCtrl.create)
+router.delete('/:id', meetingsCtrl.delete)
+router.get('/:id/edit', meetingsCtrl.edit)
+router.put('/:id', meetingsCtrl.update)
 
 module.exports = router
---
+++ routes/notes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+notesCtrl = require('../controllers/notes')
+
+module.exports = router
